77645b65386a918035356004edda477c5eb416ad
tavisca-sdeshmukh <sdeshmukh@tavisca.com>
2024-08-01 14:27:04 -0500

    broken model

--- cloud-clipboard/src/app/dashboard/dashboard.component.ts
+++ cloud-clipboard/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,25 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, inject,model, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthGoogleService } from '../services/auth-google.service';
 import { ClipBoardService } from '../services/cboard.service';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon';
+import {FormsModule} from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
 import { ClipBoardItem } from '../model/ClipBoardItem';
 
 const MODULES = [CommonModule];
@@ -11,9 +27,20 @@ const MODULES = [CommonModule];
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MODULES, MatTableModule],
+  imports: [
+    MODULES,
+    MatTableModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent implements OnInit {
   private authService = inject(AuthGoogleService);
@@ -24,6 +51,9 @@ export class DashboardComponent implements OnInit {
   clipBoardData: any;
   dataSource: any[] = [];
   displayedColumns: string[] = ['createdatetime', 'value'];
+  private dialog = inject(MatDialog);
+  readonly clipboardItemSignal = signal('');
+  readonly value = model('');
 
   ngOnInit(): void {
     this.showData();
@@ -47,5 +77,29 @@ export class DashboardComponent implements OnInit {
   logOut() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {value: this.value, clipboardItem: this.clipboardItemSignal()},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      if (result !== undefined) {
+        this.clipboardItemSignal.set(result);
+      }
+    });
+  }
+
+}
+
+export class DialogOverviewExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
+  readonly data = inject<ClipBoardItem>(MAT_DIALOG_DATA);
+  readonly clipboardItem = model(this.data.value);
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
